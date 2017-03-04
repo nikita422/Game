4ea5a6d5351aa7f48a78fe6098238347b3fa3331
Nikita <natalogic1@yandex.ru>
2017-03-04 22:42:44 +0300

    Added editor

--- Assets/myEditor/NewBehaviourScript.cs
+++ Assets/myEditor/NewBehaviourScript.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+public class NewBehaviourScript : MonoBehaviour
+{
 
-public class NewBehaviourScript : MonoBehaviour {
 
+
+    GameObject parent;
     /*
      Кнопка открывающая меню
          
@@ -16,48 +20,105 @@ public class NewBehaviourScript : MonoBehaviour {
 
     public GameObject curBlock;
 
-    void Start () {
-		
-	}
-    public void print(string _name)
+    void Start()
     {
-        switch (_name)
-        {
-            case "1":
-                {
-                     
-                }
-                break;
-
-            case "2":
-                break;
-            case "3":
-                break;
-
-
-            default:
-                break;
-        }
+        parent = GameObject.Find("0");
     }
 
-    
-	
- 	void Update () {
+
+
+    public void instance(int _n)
+    {
+        if (curBlock) Destroy(curBlock);
+        print("ShipsBlock/" + _n.ToString());
+        curBlock = Instantiate((GameObject)Resources.Load("ShipsBlock/" + _n.ToString(), typeof(GameObject)), parent.transform);
+        
+    }
+
+
+
+    void Update()
+    {
+        
 
         if (curBlock)
         {
-            curBlock.transform.position = Vector2.Lerp(curBlock.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition),0.5f);
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 intPos = new Vector2(Mathf.CeilToInt(pos.x), Mathf.CeilToInt(pos.y));
+            //   curBlock.transform.position = Vector2.Lerp(curBlock.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition),0.5f);
+            curBlock.transform.position = intPos;
+
         }
-        /*
-         расчет size и сетка тут
+
+        if (Input.GetMouseButtonUp(0) && curBlock)
+        {
+            curBlock = null;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject() && !curBlock)
+        {
+            float kZoom = Camera.main.orthographicSize;
 
 
-
+            Vector3 moupos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mouposz0 = new Vector3(moupos.x, moupos.y, 0);
         
-         
-         
-         
-         */
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(mouposz0, kZoom / 6);//what about layers, bitch?
+            if (hitColliders.Length != 0)
+            {
 
-	}
+                if (hitColliders.Length > 1)
+                {
+                    Collider2D col;
+                    float dist = Vector2.Distance(hitColliders[0].transform.position, mouposz0);
+                    col = hitColliders[0];
+
+                    for (int i = 1; i < hitColliders.Length; i++)
+                    {
+                        float newDist = Vector2.Distance(hitColliders[i].transform.position, mouposz0);
+                        if (dist > newDist)
+                        {
+                            col = hitColliders[i];
+                            dist = newDist;
+                        }
+                    }
+                    hitColliders[0] = col;
+                }
+                curBlock = hitColliders[0].gameObject;
+            }
+            print(hitColliders[0]);
+
+        }
+    }
+
+    /*
+     * 
+     * 1_1x
+     * 1_1y
+     * 1_2
+     * 1_3
+     * 
+     * 
+     * 
+     */
+
+
+
+    public void save()
+    {
+        int valueOneType, valueTwoType;
+
+        GameObject[] blocks= GameObject.FindGameObjectsWithTag("EditorOnly");
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            string name = blocks[i].name.Substring(0,1); //get type + number-------------
+            switch( name){
+
+            }
+        }
+    }
+    public void load()
+    {
+
+    }
 }
